9e41465a6ea7cff5d7d07ea19475ccc7946d21ff
U-VARONIS\gshmunik <gshmunik@GSHMUNIK-PC.varonis.com>
2017-10-11 12:16:56 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// adding bootstrap components
+import { ModalModule } from 'ngx-bootstrap/modal';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +10,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // bootstrap
+    ModalModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
